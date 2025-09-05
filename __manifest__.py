5f13e2b692fc071eca6f77834b257d5dc99aef2b
vishal <vishalp.brainerhub@gmail.com>
2025-09-05 18:24:45 +0530

    updated changes

--- __manifest__.py
+++ __manifest__.py
@@ -53,6 +53,7 @@
     
     'data': [
         'security/ir.model.access.csv',
+        'security/carddecks_access.xml',
         'data/subscription_plan_data.xml',
         'data/recaptcha_data.xml',
         'data/cron_jobs.xml',
